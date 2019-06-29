86e70d1c2c51804d852e6cac14177f8d073c4acf
YoshYoshiYosh <yoshikik@live.jp>
2019-06-30 02:26:42 +0900

    [ビューとルーティング] ナビゲーションバーを追加、mapsのresourcesから:showを除外

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   
-  resources :maps do
+  resources :maps, except: [:show] do
     get :mymap, on: :collection
     resources :pins
   end
