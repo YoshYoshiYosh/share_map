58bf8f75a502a2e259a6857ba4307fc96bc2de9f
YoshYoshiYosh <yoshikik@live.jp>
2019-07-10 00:18:03 +0900

    [RSpec] AuthorizedMapモデルのテストがパスするように修正

--- spec/factories/authorized_maps.rb
+++ spec/factories/authorized_maps.rb
@@ -1,6 +1,16 @@
 FactoryBot.define do
   factory :authorized_map do
-    user { nil }
-    map { nil }
+    # user { User.create(
+    #   email: 'invited-user@example.com',
+    #   password: 'Invited-User',
+    #   password_confirmation: 'Invited-User'
+    # ) }
+    # map { Map.create(
+    #   title: 'test',
+    #   description: 'test',
+    #   author: User.first
+    # ) }
+    association :user
+    association :map
   end
 end
