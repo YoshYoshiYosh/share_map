0f36340f27a8909d6f60176a39dcea1d37a83257
YoshYoshiYosh <yoshikik@live.jp>
2019-08-02 03:10:49 +0900

    [RSpec]homeコントローラのテストを修正

--- spec/views/home/index.html.slim_spec.rb
+++ spec/views/home/index.html.slim_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 RSpec.describe "home/index.html.slim", type: :view do
   it "renders home page including word 'ShareMap'" do
     render
-    assert_select "h2", :text => "ShareMap", :count => 1
+    assert_select "h2", :text => /ShareMap/, :count => 1
   end
 end
