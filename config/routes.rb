deccefc1e090198c3d6763e8ec927ba11b965ed2
YoshYoshiYosh <yoshikik@live.jp>
2019-08-03 13:58:31 +0900

    [AuthorizedMap] テス用データを削除する用のdeleteアクションと、そのメソッドを呼び出すリンクを実装

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   
   resources :maps do
     get :mymap, on: :collection
-    resources :authorized_maps, only: [:index, :new, :create, :update], as: 'authorize', path: 'authorizing'
+    resources :authorized_maps, only: [:index, :new, :create, :update, :destroy], as: 'authorize', path: 'authorizing'
     resources :pins
   end
   
