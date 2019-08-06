47147b0b646b0f6796e35096a1af19839bd1c928
YoshYoshiYosh <yoshikik@live.jp>
2019-08-06 22:27:39 +0900

    [画面修正：adminアクション追加] あるマップの情報を確認するためのadminページを作成(:member)
    admin.html.slimを作成し、 show.html.slim, application.scssを修正した。
    また、map.jsを修正し、adminページにて「各項目にmouseoverするとボタンを表示する」という動作を追記した。

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -96,77 +96,79 @@ async function mapInit(lons, lats) {
 
 document.addEventListener("turbolinks:load", async function(){
   console.log('読み込まれました');
-
-  let showEditButtonAtAdminPages = [document.querySelector('.manage-title'), document.querySelector('.manage-description'), document.querySelector('.manage-users'), document.querySelector('.manage-pins')];
   
-  showEditButtonAtAdminPages.forEach((element) => {
-    element.addEventListener('mouseover', showEditButton, false);
-    element.addEventListener('mouseleave', hideEditButton, false)
-  });
-
-  // マップ読み込み
-  let lonsRaw = document.querySelectorAll(".lon");
-  let latsRaw = document.querySelectorAll(".lat") ; 
+  if (/maps\/\d\/admin$/.test(location.href)) {
+    let showEditButtonAtAdminPages = [document.querySelector('.manage-title'), document.querySelector('.manage-description'), document.querySelector('.manage-users'), document.querySelector('.manage-pins')];
   
-  let pinsNumber = lonsRaw.length;
-  let lons = [];
-  let lats = [];
+    showEditButtonAtAdminPages.forEach((element) => {
+      element.addEventListener('pointerover', showEditButton, false);
+      element.addEventListener('pointerleave', hideEditButton, false);
+    });
+  }
 
-  for (let i = 0; i < pinsNumber; i++) {
-    lons.push(lonsRaw[i].textContent);
-    lats.push(latsRaw[i].textContent);
-  };
-  
-  await mapInit(lons, lats);
-
-  // テスト用のPin
-  let sightSeeing = [
-    { country: 'USA', name: 'Grand Canyon National Park', lonlat: [36.0922146, -113.4035967]},
-    { country: '日本', name: '富士山', lonlat: [35.3606422,138.7186086] },
-    { country: '中國', name: '慕田峪长城', lonlat: [40.4319118,116.5681862]},
-    { country: 'France', name: 'Tour Eiffel', lonlat: [48.8583701, 2.2922926]},
-  ];
-
-  // JavaScriptでPOSTする
-  let addPinButton = document.querySelector('.add-pin');
-  addPinButton.addEventListener('click', async () => {
-    for (let i = 0; i < sightSeeing.length; i++) {
-        // CSRF用のトークン
-        const token = document.getElementsByName('csrf-token').item(0).content;
-
-        // ボディを作る
-        const formData = new FormData();
-
-        // 成功する
-        formData.append('authenticity_token', token);
-        formData.append('pin[title]', `${sightSeeing[i].name}`);
-        formData.append('pin[description]', `${sightSeeing[i].country}の世界遺産ですよ。`);
-        formData.append('pin[lonlat]', `${sightSeeing[i].lonlat[0]} ${sightSeeing[i].lonlat[1]}`);
-        // 
-
-        const postRequest = await fetch(location.href + '/pins.json', {
-          method: "POST",
-          body: formData
-        });
-
-        console.log(postRequest);
-
-        if (postRequest.status === 200) {
-          console.log('成功');
-        } else {
-          console.log('失敗');
-        }
-    }
-
-  });
-
-  let addMemberButton = document.querySelector('.add-member');
-  addMemberButton.addEventListener('click', async () => {
+  if(/maps\/\d$/.test(location.href)) {
+    // マップ読み込み
+    let lonsRaw = document.querySelectorAll(".lon");
+    let latsRaw = document.querySelectorAll(".lat") ; 
     
-    // document.querySelector('.add-menber-form').classList.add("shown");
-    open('http://localhost:3000/maps/1/authorized_maps/new', '_blank');
+    let pinsNumber = lonsRaw.length;
+    let lons = [];
+    let lats = [];
 
-  })
+    for (let i = 0; i < pinsNumber; i++) {
+      lons.push(lonsRaw[i].textContent);
+      lats.push(latsRaw[i].textContent);
+    };
 
+    await mapInit(lons, lats);
+
+    // テスト用のPin
+    let sightSeeing = [
+      { country: 'USA', name: 'Grand Canyon National Park', lonlat: [36.0922146, -113.4035967]},
+      { country: '日本', name: '富士山', lonlat: [35.3606422,138.7186086] },
+      { country: '中國', name: '慕田峪长城', lonlat: [40.4319118,116.5681862]},
+      { country: 'France', name: 'Tour Eiffel', lonlat: [48.8583701, 2.2922926]},
+    ];
+
+    // JavaScriptでPOSTする
+    let addPinButton = document.querySelector('.add-pin');
+    addPinButton.addEventListener('click', async () => {
+      for (let i = 0; i < sightSeeing.length; i++) {
+          // CSRF用のトークン
+          const token = document.getElementsByName('csrf-token').item(0).content;
+
+          // ボディを作る
+          const formData = new FormData();
+
+          // 成功する
+          formData.append('authenticity_token', token);
+          formData.append('pin[title]', `${sightSeeing[i].name}`);
+          formData.append('pin[description]', `${sightSeeing[i].country}の世界遺産ですよ。`);
+          formData.append('pin[lonlat]', `${sightSeeing[i].lonlat[0]} ${sightSeeing[i].lonlat[1]}`);
+          // 
+
+          const postRequest = await fetch(location.href + '/pins.json', {
+            method: "POST",
+            body: formData
+          });
+
+          console.log(postRequest);
+
+          if (postRequest.status === 200) {
+            console.log('成功');
+          } else {
+            console.log('失敗');
+          }
+      }
+
+    });
+
+    let addMemberButton = document.querySelector('.add-member');
+    addMemberButton.addEventListener('click', async () => {
+      
+      // document.querySelector('.add-menber-form').classList.add("shown");
+      open('http://localhost:3000/maps/1/authorized_maps/new', '_blank');
+    })
+  }
 
 });
