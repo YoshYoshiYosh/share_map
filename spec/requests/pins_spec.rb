b08b21a83e88b755605d89efbc06ae7d8bbfe097
YoshYoshiYosh <yoshikik@live.jp>
2019-08-11 23:07:38 +0900

    [RSpec]spec/requests/pins_spec.rb 修正

--- spec/requests/pins_spec.rb
+++ spec/requests/pins_spec.rb
@@ -6,13 +6,17 @@ RSpec.describe "Pins", type: :request do
   
   let(:user) { FactoryBot.create(:user) }
   let(:map) { FactoryBot.create(:map, author: user)}
-  let(:same_author_pin) { FactoryBot.create(:pin, :same_author, map: map, author: user) }
+  let(:first_pin) { FactoryBot.create(:pin, :same_author, map: map, author: user) }
+  let(:second_pin) { FactoryBot.create(:pin, :same_author, map: map, author: user) }
+  let(:third_pin) { FactoryBot.create(:pin, :same_author, map: map, author: user) }
+  let(:pins) {[ first_pin, second_pin, third_pin ]}
 
   describe "GET maps/:map_id/pins" do
     it "works! (now write some real specs)" do
       user.confirm
       sign_in user
-      get map_pins_path([map, same_author_pin])
+      # get map_pins_path([map, same_author_pin])
+      get map_pins_path([map, pins])
       expect(response).to have_http_status(200)
     end
   end
