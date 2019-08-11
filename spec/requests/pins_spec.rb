577f78c24472251972a6c5280eaa5d1bf81c3e3a
YoshYoshiYosh <yoshikik@live.jp>
2019-08-11 23:09:36 +0900

    [RSpec]修正

--- spec/requests/pins_spec.rb
+++ spec/requests/pins_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe "Pins", type: :request do
       user.confirm
       sign_in user
       # get map_pins_path([map, same_author_pin])
-      get map_pins_path([map, pins])
+      get map_pins_path(map)
       expect(response).to have_http_status(200)
     end
   end
