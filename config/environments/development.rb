5ba589f4c1656c289b0a4921a00e73fc96c56472
YoshYoshiYosh <yoshikik@live.jp>
2019-08-25 14:36:10 +0900

    [ActiveStorage] json.jbuilderを、partialを利用するものに変更
    lonlatについても、jsonで取得出来るように変更できた

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,6 +28,7 @@ Rails.application.configure do
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options)
+  # amazonに直す
   config.active_storage.service = :local
   
 
