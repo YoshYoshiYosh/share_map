a4b97cb2dade4b536ec620ff9bc0f1b85f344ea0
YoshYoshiYosh <yoshikik@live.jp>
2019-09-16 00:22:33 +0900

    [RSpec:pins/index.html] レイアウト修正によりエラーが発生していたためテストケースを修正

--- spec/views/pins/index.html.slim_spec.rb
+++ spec/views/pins/index.html.slim_spec.rb
@@ -15,10 +15,12 @@ RSpec.describe "pins/index", type: :view do
 
   it "renders a list of pins" do
     render
-    assert_select "tr>th", :text => "Author".to_s, :count => 1
-    assert_select "tr>th", :text => "Title".to_s, :count => 1
-    assert_select "tr>th", :text => "Description".to_s, :count => 1
-    assert_select "tr>th", :text => "Lonlat".to_s, :count => 1
-    assert_select "tr>td", :text => /test-\d@example.com/, :count => 2
+
+    time = pins.first.created_at
+
+    assert_select "div>small", :text => "ひとこと： test1-description", :count => 1
+    assert_select "div>small", :text => "作成者： test-1@example.com", :count => 2
+    assert_select "div>small", :text => "作成日時： #{time}", :count => 2
+    assert_select "h1>a", :text => "＜ マップ管理画面へ戻る".to_s, :count => 1
   end
 end
