e6846b277a77c73a3282c04b726404abb7fba4c6
YoshYoshiYosh <yoshikik@live.jp>
2019-09-22 22:48:01 +0900

    [contacts_controller.rb] before_actionとして:authenticate_user!を追記した

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,14 +1,9 @@
 class ContactsController < ApplicationController
-
-  # https://www.javadrive.jp/rails/controller/index6.html
+  before_action :authenticate_user!
   
   def create
-    if user_signed_in?
       opinion = params[:contact]
-      # flash[:success] = "#{opinion}!!"
-      puts "#{opinion}"
       ContactMailer.with(user: current_user, opinion: opinion).contact_arrived_mail.deliver_now
-      redirect_to root_url
-    end
+      redirect_to root_url # JavaScriptにてレンダリングするため、この行は何もしない
   end
 end
