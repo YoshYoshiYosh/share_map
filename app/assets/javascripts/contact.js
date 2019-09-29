2135a144efebbd52b99e2de692a611f60802d497
YoshYoshiYosh <yoshikik@live.jp>
2019-09-29 15:58:10 +0900

    [eslint] application.js, cable.js, contact.js, map.jsをlintツールにて修正

--- app/assets/javascripts/contact.js
+++ app/assets/javascripts/contact.js
@@ -1,41 +1,39 @@
 async function sendContactMessage() {
-  let opinionText = document.querySelector('.opinion-text').value;
+  const opinionText = document.querySelector('.opinion-text').value;
 
   const token = document.getElementsByName('csrf-token').item(0).content;
 
   const formData = new FormData();
   formData.append('authenticity_token', token);
-  formData.append("contact", opinionText);
+  formData.append('contact', opinionText);
 
   // 開発環境と本番環境のどちらでも稼働させるために記載(環境によってPOST先を変更する必要があるため)
-  let tmpVariableProtocol = /^https/.test(location.href) ? "https://" : "http://";
-  
-  const postRequest = await fetch(tmpVariableProtocol + location.host + '/contact', {
-    method: "POST",
+  const tmpVariableProtocol = /^https/.test(location.href) ? 'https://' : 'http://';
+
+  const postRequest = await fetch(`${tmpVariableProtocol + location.host}/contact`, {
+    method: 'POST',
     body: formData,
-    redirect: "manual"
+    redirect: 'manual',
   });
 
-  console.log(postRequest)
+  console.log(postRequest);
 
   if (postRequest.status === 201) {
     console.log('成功');
-    setFlash("success");
+    setFlash('success');
   } else {
     console.log('失敗');
     console.log(postRequest.status);
-    setFlash("danger");
-  };
+    setFlash('danger');
+  }
 
   setTimeout(() => {
     const flashElement = document.querySelector('.alert');
     flashElement.parentNode.removeChild(flashElement);
   }, 5000);
-
 }
 
-document.addEventListener("turbolinks:load", async () => {
-  let sendOpinionButton = document.getElementById('send-opinion');
+document.addEventListener('turbolinks:load', async () => {
+  const sendOpinionButton = document.getElementById('send-opinion');
   sendOpinionButton.addEventListener('click', sendContactMessage);
-
 });
--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,211 +1,201 @@
-function escapeHtml (string) {
-  return string.replace(/[&'`"<>]/g, function(match) {
-    return {
-      '&': '&amp;',
-      "'": '&#x27;',
-      '`': '&#x60;',
-      '"': '&quot;',
-      '<': '&lt;',
-      '>': '&gt;',
-    }[match]
-  });
-};
+function escapeHtml(string) {
+  return string.replace(/[&'`"<>]/g, (match) => ({
+    '&': '&amp;',
+    "'": '&#x27;',
+    '`': '&#x60;',
+    '"': '&quot;',
+    '<': '&lt;',
+    '>': '&gt;',
+  }[match]));
+}
 
 function showEditButton(e) {
   e.target.firstElementChild.classList.add('shown');
-};
+}
 
 function hideEditButton(e) {
   e.target.firstElementChild.classList.remove('shown');
-};
+}
 
 function setLonlat() {
   return new Promise((resolve, reject) => {
-    navigator.geolocation.getCurrentPosition(pos => {
-      resolve({lat: pos.coords.latitude, lon: pos.coords.longitude})
+    navigator.geolocation.getCurrentPosition((pos) => {
+      resolve({ lat: pos.coords.latitude, lon: pos.coords.longitude });
     },
-    err => {
-      reject(err)
+    (err) => {
+      reject(err);
     },
     {
-      enableHighAccuracy: true
-    })
-  })
+      enableHighAccuracy: true,
+    });
+  });
 }
 
 function adjustMarginWhenPinCreated() {
-  successFlash = document.querySelector('.alert-success') || ''
+  successFlash = document.querySelector('.alert-success') || '';
   if (successFlash) {
-    return true
-  };
-};
+    return true;
+  }
+}
 
 function setFlash(successOrDanger) {
   const flashWrapper = document.querySelector('.nav-flash');
-  const flashElement = document.createElement("p");
+  const flashElement = document.createElement('p');
 
-  if (successOrDanger === "success") {
-    var message = document.createTextNode("貴重なご意見をありがとうございます。サービス向上のため、活用させていただきます。"); 
+  if (successOrDanger === 'success') {
+    var message = document.createTextNode('貴重なご意見をありがとうございます。サービス向上のため、活用させていただきます。');
   } else {
-    var message = document.createTextNode("アカウント登録もしくはログインしてください。");
-  };
+    var message = document.createTextNode('アカウント登録もしくはログインしてください。');
+  }
   flashElement.appendChild(message);
-  flashElement.classList.add('alert',`alert-${successOrDanger}`);
+  flashElement.classList.add('alert', `alert-${successOrDanger}`);
   flashWrapper.appendChild(flashElement);
-};
+}
 
 async function mapInit(location) {
+  const isAuthor = !!document.getElementById('is-author');
 
-  let isAuthor = !!document.getElementById('is-author');
+  const storedPins = await fetch(`${location}/pins.json`)
+    .then((response) => response.json());
 
-  let storedPins = await fetch(`${location}/pins.json`)
-    .then(function(response) {
-      return response.json();
-    });
-
-  if(storedPins.length === 0) {
-    storedPins.push({ title: 'default', lonlat: { x: 51.476853, y: -0.0005002 } })
-  };
+  if (storedPins.length === 0) {
+    storedPins.push({ title: 'default', lonlat: { x: 51.476853, y: -0.0005002 } });
+  }
 
   map = L.map('mapid').setView([storedPins[0].lonlat.x, storedPins[0].lonlat.y], 5);
 
-  let pinIcon = L.icon({
+  const pinIcon = L.icon({
     iconUrl: '/pin_icon.png',
     iconSize: [30, 30],
     iconAnchor: [15, 30],
-    popupAnchor: [0, -40]
+    popupAnchor: [0, -40],
   });
 
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(map);
 
   L.Control.Watermark = L.Control.extend({
-    onAdd: function(map) {
+    onAdd(map) {
+      const outerDiv = L.DomUtil.create('div', 'map-button-container d-flex');
 
-      let outerDiv = L.DomUtil.create('div', "map-button-container d-flex");
-      
       outerDiv.style.backgroundColor = 'rgba(255,255,255,.5)';
       outerDiv.style.padding = '20px';
       outerDiv.style.margin = '0';
       outerDiv.style.zIndex = '5';
 
-      let imageAndTextOfButton = [
-        { imgSrc: '/single_pin_icon.svg', text: 'New Pin'  },
+      const imageAndTextOfButton = [
+        { imgSrc: '/single_pin_icon.svg', text: 'New Pin' },
         { imgSrc: '/destination_icon.svg', text: 'New Pin' },
-        { imgSrc: '/human_pin_icon.svg', text: 'View Pins' }
+        { imgSrc: '/human_pin_icon.svg', text: 'View Pins' },
       ];
-      
+
       if (isAuthor) {
         imageAndTextOfButton.push({ imgSrc: '/teams_icon.svg', text: 'Add Member' });
-      };
+      }
 
       for (let i = 0; i < imageAndTextOfButton.length; i++) {
-        let div = L.DomUtil.create('div', "map-button-container__box", outerDiv);
-        let img = L.DomUtil.create('img', "pin-icon", div);   
-        
-        switch(i) {
+        const div = L.DomUtil.create('div', 'map-button-container__box', outerDiv);
+        const img = L.DomUtil.create('img', 'pin-icon', div);
+
+        switch (i) {
           case 0: {
-            img.classList.add("add-pin");
+            img.classList.add('add-pin');
             img.setAttribute('data-toggle', 'modal');
             img.setAttribute('data-target', '#exampleModalCenter');
             break;
           }
 
           // case 1:は、現在地を取得するピン作成？
-          
+
           case 2: {
-            img.classList.add("add-menber-form");
+            img.classList.add('add-menber-form');
             break;
           }
 
           case 3: {
-            img.classList.add("add-member");
+            img.classList.add('add-member');
             break;
           }
         }
 
-        let smallText = L.DomUtil.create('p', "small-text", div);
+        const smallText = L.DomUtil.create('p', 'small-text', div);
 
         smallText.textContent = imageAndTextOfButton[i].text;
         img.src = imageAndTextOfButton[i].imgSrc;
-
-      };
+      }
       return outerDiv;
     },
 
-    onRemove: function(map) {
-        // Nothing to do here
-    }
+    onRemove(map) {
+      // Nothing to do here
+    },
   });
-  
-  L.control.watermark = function(opts) {
-      return new L.Control.Watermark(opts);
+
+  L.control.watermark = function (opts) {
+    return new L.Control.Watermark(opts);
   };
-  
+
   L.control.watermark({ position: 'topright' }).addTo(map);
-  
+
   for (let i = 0; i < storedPins.length; i++) {
     if (storedPins[i].image === undefined) {
       L.marker([storedPins[i].lonlat.x, storedPins[i].lonlat.y]).addTo(map)
-      .bindPopup(`This is <br><h3>${escapeHtml(storedPins[i].title)}</h3>`)
-      .openPopup();
+        .bindPopup(`This is <br><h3>${escapeHtml(storedPins[i].title)}</h3>`)
+        .openPopup();
     } else {
       L.marker([storedPins[i].lonlat.x, storedPins[i].lonlat.y]).addTo(map)
-      .bindPopup(`This is <br><h3>${escapeHtml(storedPins[i].title)}</h3><img class="pin-image" src=${storedPins[i].image}>`)
-      .openPopup();
-    };
-  };
+        .bindPopup(`This is <br><h3>${escapeHtml(storedPins[i].title)}</h3><img class="pin-image" src=${storedPins[i].image}>`)
+        .openPopup();
+    }
+  }
+}
 
-};
-
-document.addEventListener("turbolinks:load", async function(){
+document.addEventListener('turbolinks:load', async () => {
   console.log('読み込まれました');
-  
+
   if (/maps\/\d\/admin\/?$/.test(location.href)) {
-    let showEditButtonAtAdminPages = [
-                                      document.querySelector('.manage-title'),
-                                      document.querySelector('.manage-description'),
-                                      document.querySelector('.manage-users'),
-                                      document.querySelector('.manage-pins')
-                                    ];
-  
+    const showEditButtonAtAdminPages = [
+      document.querySelector('.manage-title'),
+      document.querySelector('.manage-description'),
+      document.querySelector('.manage-users'),
+      document.querySelector('.manage-pins'),
+    ];
+
     showEditButtonAtAdminPages.forEach((element) => {
       element.addEventListener('pointerover', showEditButton, false);
       element.addEventListener('pointerleave', hideEditButton, false);
     });
-  };
+  }
 
-  if(/maps\/\d\/?$/.test(location.href)) {
-
+  if (/maps\/\d\/?$/.test(location.href)) {
     if (adjustMarginWhenPinCreated()) {
       document.querySelector('.breadcrumb').classList.replace('mt-0', 'flash-fix-margin');
-    };
-    
+    }
+
     await mapInit(location.href);
 
-    let addMemberButton = document.querySelector('.add-member');
+    const addMemberButton = document.querySelector('.add-member');
     addMemberButton.addEventListener('click', () => {
       open(`${location.href}/authorized_maps/new`);
-    })
+    });
 
-    let addPinButton = document.querySelector('.add-pin');
+    const addPinButton = document.querySelector('.add-pin');
     addPinButton.addEventListener('click', async () => {
       const latlon = await setLonlat();
-      console.log(`lat:${latlon.lat}, lon:${latlon.lon}`)
+      console.log(`lat:${latlon.lat}, lon:${latlon.lon}`);
 
-      let submitButton = document.getElementsByName('commit')[0]
+      const submitButton = document.getElementsByName('commit')[0];
       submitButton.addEventListener('click', async (event) => {
+        console.log('送信ボタンがクリックされました');
 
-        console.log("送信ボタンがクリックされました")
-        
         event.preventDefault();
-        
+
         const token = document.getElementsByName('csrf-token').item(0).content;
 
-        let inputTitle       = document.getElementById("pin_title").value;
-        let inputDescription = document.getElementById("pin_description").value;
-        let inputLonlat      = `${latlon.lat} ${latlon.lon}`;
+        const inputTitle = document.getElementById('pin_title').value;
+        const inputDescription = document.getElementById('pin_description').value;
+        const inputLonlat = `${latlon.lat} ${latlon.lon}`;
 
         const formData = new FormData();
         formData.append('authenticity_token', token);
@@ -213,23 +203,19 @@ document.addEventListener("turbolinks:load", async function(){
         formData.append('pin[description]', inputDescription);
         formData.append('pin[lonlat]', inputLonlat);
 
-        const postRequest = await fetch(location.href + '/pins', {
-          method: "POST",
-          body: formData
+        const postRequest = await fetch(`${location.href}/pins`, {
+          method: 'POST',
+          body: formData,
         });
 
         if (postRequest.status === 200) {
           console.log('成功');
-          location.reload()
+          location.reload();
         } else {
           console.log('失敗');
           console.log(postRequest.status);
         }
-        
-      }
-      )
-    })
-
+      });
+    });
   }
-
 });
