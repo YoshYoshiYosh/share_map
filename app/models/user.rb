d50e2c392771c0bae195122c096cd8cf97ad1cdc
YoshYoshiYosh <yoshikik@live.jp>
2019-10-05 18:01:45 +0900

    [Userモデルにavatarカラムを導入]

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_one_attached :avatar
   has_many :maps, inverse_of: 'author', foreign_key: 'author_id'
   has_many :pins, inverse_of: 'author', foreign_key: 'author_id'
-
   has_many :authorized_maps, foreign_key: 'user_id'
   has_many :can_edit_maps, through: :authorized_maps, source: :map
 
