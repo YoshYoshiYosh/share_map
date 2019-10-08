c0778dd18cfbad9da6101adfe57b053b2ae82f7a
YoshYoshiYosh <yoshikik@live.jp>
2019-10-09 00:46:53 +0900

    [application_controller.rb, registrations/new.html.slim] 微修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:avatar])
-  end 
+    devise_parameter_sanitizer.permit(:account_update, keys: [:avatar])
+  end
   
 end
