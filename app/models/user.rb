5c0369f18c1b53be954bdb007b58d6c88c142cf8
YoshYoshiYosh <yoshikik@live.jp>
2019-10-09 00:46:08 +0900

    [models/user.rb] avatarを必須項目として設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,16 @@ class User < ApplicationRecord
   has_many :pins, inverse_of: 'author', foreign_key: 'author_id'
   has_many :authorized_maps, foreign_key: 'user_id'
   has_many :can_edit_maps, through: :authorized_maps, source: :map
+  
+  validates :force_avatar_attached, presence: true
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+
+  def force_avatar_attached
+    errors.add(:avatar, 'は添付必須項目です') unless avatar.attached?
+  end
+  
 end
