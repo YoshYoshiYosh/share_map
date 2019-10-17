59c2f2abc060bec2e0ff85b4c7a84950c7481d19
YoshYoshiYosh <yoshikik@live.jp>
2019-10-17 23:18:45 +0900

    [pin.rb] jsonで返す値をカスタマイズするためのメソッドを実装

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,10 +1,23 @@
 # frozen_string_literal: true
 
 class Pin < ApplicationRecord
+  include Rails.application.routes.url_helpers
   has_one_attached :image
   belongs_to :author, class_name: 'User'
   belongs_to :map
 
   validates :title, presence: true
   validates :lonlat, presence: true
+
+  def custom_lonlat
+    {
+      x: self.lonlat.x,
+      y: self.lonlat.y,
+    }
+  end
+  
+  def custom_image
+    url_for(self.image) if self.image.attached?
+  end
+  
 end
