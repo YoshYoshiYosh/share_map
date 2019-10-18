344483166fdc78318ccb6bd41eb95a2b60cbbf8e
YoshYoshiYosh <yoshikik@live.jp>
2019-10-18 15:38:47 +0900

    [factories/users.rb] avatar属性として、ファイルをアップロードするよう修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,6 @@ FactoryBot.define do
   factory :user, aliases: [:author] do
     sequence(:email) { |n| "test-#{n}@example.com" }
     password { 'Password' }
+    avatar { fixture_file_upload(Rails.root.join('spec', 'support', 'assets', 'test-image.png'), 'image/png') }
   end
 end
