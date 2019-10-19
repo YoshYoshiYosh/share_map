df867b72200d5366d55df8f2f4c4ad58227386d8
YoshYoshiYosh <yoshikik@live.jp>
2019-10-19 21:24:09 +0900

    [factories/users.rb] avatarカラムにファイルをアタッチするよう修正()

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,6 +4,9 @@ FactoryBot.define do
   factory :user, aliases: [:author] do
     sequence(:email) { |n| "test-#{n}@example.com" }
     password { 'Password' }
-    avatar { fixture_file_upload(Rails.root.join('spec', 'support', 'assets', 'test-image.png'), 'image/png') }
+    # avatar { fixture_file_upload(Rails.root.join('spec', 'support', 'assets', 'test-image.png'), 'image/png') }
+    before(:create) do |user|
+      user.avatar.attach(io: File.open(Rails.root.join('spec', 'support', 'assets', 'test-image.png')), filename: "test-image.png", content_type: "image/png")
+    end
   end
 end
