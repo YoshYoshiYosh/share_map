ccdd73968fd341ae4884688f792a98c3fd0946c8
YoshYoshiYosh <yoshikik@live.jp>
2019-10-20 20:52:53 +0900

    [fix] fix too many line

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -5,7 +5,11 @@ FactoryBot.define do
     sequence(:email) { |n| "test-#{n}@example.com" }
     password { 'Password' }
     before(:create) do |user|
-      user.avatar.attach(io: File.open(Rails.root.join('spec', 'support', 'assets', 'test-image.png')), filename: "test-image.png", content_type: "image/png")
+      user.avatar.attach(
+        io: File.open(Rails.root.join('spec', 'support', 'assets', 'test-image.png')),
+        filename: "test-image.png",
+        content_type: "image/png"
+      )
     end
   end
 end
