eca1e9da987cc8ba9591ea00869d7dfe62d01c47
YoshYoshiYosh <yoshikik@live.jp>
2019-10-20 20:46:02 +0900

    [development.rb, application.rb] default_url_optionsを環境に応じて使い分けるよう修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+Rails.application.routes.default_url_options[:host] = 'localhost'
+Rails.application.routes.default_url_options[:port] = 3000
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -28,7 +31,7 @@ Rails.application.configure do
 
     config.cache_store = :null_store
   end
-
+  
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
