b1d8a04e5382a7e994b1d0dc1098e2afa3178182
YoshYoshiYosh <yoshikik@live.jp>
2019-10-20 22:19:46 +0900

    delete old default_url_options

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -52,8 +52,6 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-  config.default_url_options = { host: 'localhost:3000' }
-
   # Debug mode disables concatenation and preprocessing of assets.
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
