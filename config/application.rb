c40b928cd8742e041b1e0ecb1a65e33cff76a62b
YoshYoshiYosh <yoshikik@live.jp>
2019-10-20 21:28:16 +0900

    fix application.rb

--- config/application.rb
+++ config/application.rb
@@ -24,9 +24,6 @@ module ShareMap
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
-
-    Rails.application.routes.default_url_options[:host] = 'localhost'
-    Rails.application.routes.default_url_options[:port] = 3000
     
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
