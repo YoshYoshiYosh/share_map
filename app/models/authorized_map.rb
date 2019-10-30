cda07248adc940d2a3f6df140d079271f737bc83
YoshYoshiYosh <yoshikik@live.jp>
2019-10-31 02:02:39 +0900

    fix method "user_valid?"

--- app/models/authorized_map.rb
+++ app/models/authorized_map.rb
@@ -12,8 +12,12 @@ class AuthorizedMap < ApplicationRecord
   private
 
   def user_valid?
-    if self.user == current_user
-      errors[:base] << '自ユーザは追加できません'
+    if user == current_user
+      errors[:base] << '自分を招待することはできません'
+    elsif map.authorized_users.include?(user)
+      errors[:base] << 'すでに招待されているユーザーです'
+    elsif User.all.exclude?(user)
+      errors[:base] << '存在しないユーザーは招待できません'
     end
   end
 end
